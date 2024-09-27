0e518ebe56df2c57ce11bec50fbc43db822744bb
Isha290411 <sathe.isha29@gmail.com>
2024-09-26 19:02:28 -0500

    Update BasicOmniOpMode_Linear3.java
    servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear3.java
@@ -33,6 +33,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
@@ -73,6 +74,9 @@ public class BasicOmniOpMode_Linear3 extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+
+    private Servo servo0;
+
 
     @Override
     public void runOpMode() {
