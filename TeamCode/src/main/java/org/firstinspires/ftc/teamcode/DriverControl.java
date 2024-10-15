10f4e86f58742c78f868508d2b6330a6b185f476
rhsftc <ronsearle@hotmail.com>
2024-10-15 14:31:13 -0500

    Made some variables final. Created Lift variable, to correct an error.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -38,31 +38,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
 
 /*
- * This file contains an example of a Linear "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
- *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
- *
- * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
- *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
- * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
- * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ * This is a teleop opMode designed to work with a RobotHardware class and the FtcLib library.
+ * The main use of FtcLib is the gamepad extensions.
  */
 
 @TeleOp(name = "Driver Control", group = "Robot")
@@ -70,13 +47,13 @@ import org.firstinspires.ftc.teamcode.subsystems.Lift;
 public class DriverControl extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
-    private ElapsedTime runtime = new ElapsedTime();
+    private final ElapsedTime runtime = new ElapsedTime();
     // Create a RobotHardware object to be used to access robot hardware.
     // Prefix any hardware functions with "robot." to access this class.
     public RobotHardware robot = new RobotHardware(this);
-    private Lift lift = null;
+    private final Lift lift = new Lift(robot);
 
-    private GamepadEx gamepadOne = new GamepadEx(gamepad1);
+    private final GamepadEx gamepadOne = new GamepadEx(gamepad1);
 
     @Override
     public void runOpMode() {
@@ -105,7 +82,7 @@ public class DriverControl extends LinearOpMode {
 
             lift.update();
             // Show the elapsed game time.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Status", "Run Time: " + runtime);
             telemetry.update();
         }
     }
