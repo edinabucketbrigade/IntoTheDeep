e486aeeecab9a106fbc4979cbfb2f27bed349b98
rhsftc <ronsearle@hotmail.com>
2024-10-23 12:14:24 -0500

    Refactored the Lift Update method.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -6,9 +6,13 @@ import org.firstinspires.ftc.teamcode.RobotHardware;
 import org.firstinspires.ftc.teamcode.enums.LiftPosition;
 
 public class Lift extends SubSystem {
-    LiftPosition liftPosition;
-    LiftPosition liftState;
-    RobotHardware robot;
+    private LiftPosition liftPosition;
+    private LiftPosition liftState;
+    private RobotHardware robot;
+    private boolean aPressed = false;
+    private boolean xPressed = false;
+    private boolean yPressed = false;
+
     // Encoder positions for the lift.
     //TODO Test to find the proper values.
     private final int LIFT_DOWN = 0;
@@ -25,6 +29,9 @@ public class Lift extends SubSystem {
     public void init() {
         liftPosition = LiftPosition.Down;
         liftState = liftPosition;
+        // Make sure encoder is 0 at start.
+        robot.liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
@@ -33,28 +40,49 @@ public class Lift extends SubSystem {
 
     @Override
     public void update() {
-        if (robot.liftMotor.isBusy() && (liftState != liftPosition)) {
-            return;
-        } else {
-            liftState = liftPosition;
+        switch (liftState) {
+            case Down:
+                if (robot.liftMotor.getCurrentPosition() < 5) {
+                    if (xPressed) {
+                        robot.liftMotor.setTargetPosition(LIFT_DOWN);
+                        liftState = LiftPosition.LowBasket;
+                    }
+
+                    if (yPressed) {
+                        robot.liftMotor.setTargetPosition(LIFT_HIGH);
+                        liftState = LiftPosition.HighBasket;
+                    }
+                }
+                break;
+            case LowBasket:
+                if (robot.liftMotor.getCurrentPosition() < 5) {
+                    if (aPressed) {
+                        robot.liftMotor.setTargetPosition(LIFT_DOWN);
+                        liftState = LiftPosition.Down;
+                    }
+
+                    if (yPressed) {
+                        robot.liftMotor.setTargetPosition(LIFT_HIGH);
+                        liftState = LiftPosition.HighBasket;
+                    }
+                }
+                break;
+            case HighBasket:
+                if (robot.liftMotor.getCurrentPosition() < 5) {
+                    if (aPressed) {
+                        robot.liftMotor.setTargetPosition(LIFT_DOWN);
+                        liftState = LiftPosition.Down;
+                    }
+                }
+                break;
+            default:
+                // If we get here we have problem, stop the lift.
+                robot.liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                robot.liftMotor.setPower(0);
+                liftState = LiftPosition.Down;
         }
 
-        // Only try to move if we are finished with any moves.
-        if (!robot.liftMotor.isBusy()) {
-            switch (liftPosition) {
-                case Down:
-                    robot.liftMotor.setTargetPosition(LIFT_DOWN);
-                case LowBasket:
-                    robot.liftMotor.setTargetPosition(LIFT_LOW);
-                case HighBasket:
-                    robot.liftMotor.setTargetPosition(LIFT_HIGH);
-            }
-
-            if (liftState != liftPosition) {
-                robot.liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robot.liftMotor.setPower(LIFT_MAX_POWER);
-            }
-        }
+        robot.liftMotor.setPower(LIFT_MAX_POWER);
     }
 
     // Respond to gamepad inputs.
