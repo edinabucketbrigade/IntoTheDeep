9d6b5c7061ca505badea99f113408d33d477a327
rhsftc <ronsearle@hotmail.com>
2024-12-03 12:21:52 -0600

    Added RR Actions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actions/ArmNeutral.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.actions;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+
+public class ArmNeutral implements Action {
+    private boolean initialized = false;
+
+    @Override
+    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+        return false;
+    }
+}
