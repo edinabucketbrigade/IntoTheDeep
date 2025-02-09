3f55e58220953937c4da1f512ab622c5e9de55c1
rhsftc <ronsearle@hotmail.com>
2025-02-09 12:41:29 -0600

    Implement driver/field centric driving using the gm0 examples.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -88,10 +88,10 @@ public class RobotHardware {
             slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
 
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
         imu = myOpMode.hardwareMap.get(IMU.class, "imu");
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
@@ -127,31 +127,35 @@ public class RobotHardware {
      * <p>
      * Positive Yaw is counter-clockwise
      */
-    public void moveRobot(double x, double y, double yaw) {
+    public void moveRobot(double y, double x, double yaw) {
         // This is the gm0 version of mecanum drive.
-        //x *= 1.1; // Adjust for imperfect strafing.
+        x *= 1.1; // Adjust for imperfect strafing.
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio,
         // but only if at least one is out of the range [-1, 1]
-        //double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(yaw), 1);
-
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(yaw), 1);
         // Calculate wheel powers.
-        double leftFrontPower = x - y - yaw;
-        double rightFrontPower = x + y + yaw;
-        double leftBackPower = x + y - yaw;
-        double rightBackPower = x - y + yaw;
+        double leftFrontPower = (y + x + yaw) / denominator;
+        double leftBackPower = (y - x + yaw) / denominator;
+        double rightFrontPower = (y - x - yaw) / denominator;
+        double rightBackPower = (y + x - yaw) / denominator;
+
+//        double leftFrontPower = x - y - yaw;
+//        double rightFrontPower = x + y + yaw;
+//        double leftBackPower = x + y - yaw;
+//        double rightBackPower = x - y + yaw;
 
         // Normalize wheel powers to be less than 1.0
-        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        max = Math.max(max, Math.abs(leftBackPower));
-        max = Math.max(max, Math.abs(rightBackPower));
-
-        if (max > 1.0) {
-            leftFrontPower /= max;
-            rightFrontPower /= max;
-            leftBackPower /= max;
-            rightBackPower /= max;
-        }
+//        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+//        max = Math.max(max, Math.abs(leftBackPower));
+//        max = Math.max(max, Math.abs(rightBackPower));
+//
+//        if (max > 1.0) {
+//            leftFrontPower /= max;
+//            rightFrontPower /= max;
+//            leftBackPower /= max;
+//            rightBackPower /= max;
+//        }
 
         // Send powers to the wheels.
         leftFrontDrive.setPower(leftFrontPower);
@@ -170,7 +174,7 @@ public class RobotHardware {
      * <p>
      * Positive Yaw is counter-clockwise
      */
-    public void moveRobot(double x, double y, double yaw, double heading) {
+    public void moveRobot(double y, double x, double yaw, double heading) {
         // Rotate the movement direction counter to the bot's rotation
         double rotX = x * Math.cos(-heading) - y * Math.sin(-heading);
         double rotY = x * Math.sin(-heading) + y * Math.cos(-heading);
